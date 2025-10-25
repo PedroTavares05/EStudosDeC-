45c41faf7fe8bbe79fea9263d895a5ec8c1d54f7
Pedro Henrique Tavares <phtsantos05@gmail.com>
2025-10-25 16:57:19 -0300

    variaveis em C#

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
-﻿using exemplosFundamentos.models;
-using exemplosFundamentos.models.pet;
-Pessoa p = new Pessoa();
+﻿//using exemplosFundamentos.models;
+//using exemplosFundamentos.models.pet;
+//Pessoa p = new Pessoa();
 
-p.Nome = "Pedro";
-p.Idade = 18;
-p.Apresentar();
+//p.Nome = "Pedro";
+//p.Idade = 18;
+//p.Apresentar();
 
 
-Pet Pepe = new Pet();
-Pepe.Raca = "labrador";
-Pepe.Idade = 5;
-Pepe.Apresentar();
+//Pet Pepe = new Pet();
+//Pepe.Raca = "labrador";
+//Pepe.Idade = 5;
+//Pepe.Apresentar();
+
+int Numero = 10;
+string Nome = "Lucas";
+bool condicao = false;
+decimal Valor = 100.80M;
+double Altura = 1.80;
+
+ Console.WriteLine("meu nome é " + Nome);
+ Console.WriteLine("o numero escolhido é:  " + Numero);
+ Console.WriteLine("A condição é: " + condicao);
+ Console.WriteLine("Sua altura é de:  " + Altura.ToString("0.00"));
+ Console.WriteLine("O valor do item é de:  " + Valor);
